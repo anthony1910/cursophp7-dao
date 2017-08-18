57d9411bf076d5dde2c60e25d46371ec19fe174d
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-08-18 11:59:46 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuario
+// $root = new Usuario();
+// $root->loadById(1);
+// echo $root;
 
-$root->loadById(1);
+// Carrega uma lista de usuarios
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("d");
+// echo json_encode($search);
+
+// Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+
+$usuario->login("anthony", "123456");
+
+echo $usuario;
 
 ?>
